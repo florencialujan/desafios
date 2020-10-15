c5b1a7e57323463f1d8b7c70d3b387b47a904faf
florencialujan <florlujaan@gmail.com>
2020-10-15 00:46:02 -0300

    Sexto commit

---
+++ OPERACIONES/OPERACIONES/Calculadora.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERACIONES
+{ 
+    class Calculadora
+    {
+
+        /*private int n1;  //COMO VAMOS A USAR METODOS ESTÁTICOS, NO NECESITAMOS CREAR ATRIBUTOS NI CONSTRUCTORES A ESTOS.
+        private int n2;
+
+        public Calculadora(int n1, int n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+
+        }
+        */
+        public static void suma(int n1, int n2)
+        {
+            int s;
+            s = n1 + n2;
+            Console.WriteLine("El resultado de la suma (n1+n2) es: " + s);
+        }
+
+        public static void resta(int n1, int n2)
+        {
+            int r;
+            r = n1 - n2;
+            Console.WriteLine("El resultado de la resta (n1-n2) es: " + r);
+        }
+
+
+    }
+}
---
+++ OPERACIONES/OPERACIONES/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERACIONES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1=0, n2=0;
+            string entrada;
+
+            Console.WriteLine("Ingrese un primer numero");
+            entrada = Console.ReadLine();
+            n1 = Convert.ToInt32(entrada);
+            Console.WriteLine("Ingrese un segundo numero");
+            entrada = Console.ReadLine();
+            n2 = Convert.ToInt32(entrada);
+          
+            Calculadora.suma(n1, n2);
+            Calculadora.resta(n1, n2);
+            Console.ReadKey();
+        }
+    }
+}
